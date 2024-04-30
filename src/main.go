3a70dda4a44f82478421577ec386f263ef2fb649
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2024-04-30 22:04:59 +0200

    Feature: implement realtime subscribtion via websocket

--- src/main.go
+++ src/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/ThomasBoom89/simple-http-message-queue/internal"
+	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"time"
 )
 
 func main() {
@@ -28,6 +31,73 @@ func main() {
 
 		return c.SendString(element)
 	})
+
+	app.Use("/ws", func(c *fiber.Ctx) error {
+		// IsWebSocketUpgrade returns true if the client
+		// requested upgrade to the WebSocket protocol.
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return c.Next()
+		}
+		return fiber.ErrUpgradeRequired
+	})
+
+	connectionPool := make(map[*websocket.Conn]bool, 10)
+	registerPool := make(chan *websocket.Conn, 10)
+	unregisterPool := make(chan *websocket.Conn, 10)
+
+	go func() {
+		for {
+			select {
+			case connection := <-registerPool:
+				connectionPool[connection] = true
+				log.Debug("connection registered")
+
+			case connection := <-unregisterPool:
+				delete(connectionPool, connection)
+				log.Debug("connection unregistered")
+			}
+		}
+	}()
+
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		// unregister connection
+		defer func() {
+			unregisterPool <- c
+			c.Close()
+		}()
+
+		// register connection
+		registerPool <- c
+		// read messages
+		for {
+			_, _, err := c.ReadMessage()
+			if err != nil {
+				if websocket.IsCloseError(err) || websocket.IsUnexpectedCloseError(err) {
+					log.Debug(err)
+				}
+				return
+			}
+		}
+	}))
+
+	// send messages to pool of connection
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+			if len(connectionPool) == 0 {
+				continue
+			}
+			message, err := queue.Dequeue()
+			if err != nil {
+				continue
+			}
+
+			for connection, _ := range connectionPool {
+				connection.WriteMessage(websocket.TextMessage, []byte(message))
+			}
+		}
+	}()
 
 	err := app.Listen(":3000")
 	if err != nil {
